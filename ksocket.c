a371f5a3d53ff6a931cf60da20ad45de361c2709
Lakshya Agrawal <155128944+laksh0820@users.noreply.github.com>
2025-06-25 18:55:55 +0530

    Update ksocket.c

--- ksocket.c
+++ ksocket.c
@@ -1,11 +1,3 @@
-/*
-    ============================================================
-    Assignment 4 Submission
-    Name: Lakshya Agrawal
-    Roll number: 22CS30036
-    ============================================================
-*/
-
 #include "ksocket.h"
 
 // Structure used to define operation on the semaphores in semaphore set
